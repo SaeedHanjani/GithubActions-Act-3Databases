b34e0e1f3767e10188042d5c8870aad4a54935a2
Saeed Hanjani <s.mehdizadehh@gmail.com>
2024-12-29 11:02:27 +0330

    enable function main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -49,13 +49,13 @@ class TestBackupRestore(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-#import unittest
-#from src.sample import add  # Adjust the import based on your project structure
+import unittest
+from src.sample import add  # Adjust the import based on your project structure
 
-#class TestSample(unittest.TestCase):
-   # def test_add(self):
-  #      self.assertEqual(add(2, 3), 5)
- #       self.assertEqual(add(-1, 1), 0)
+class TestSample(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
 
-#if __name__ == '__main__':
-#    unittest.main()
+if __name__ == '__main__':
+    unittest.main()
